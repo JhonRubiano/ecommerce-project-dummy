beb1ff1bd90f620abdb21bba303e0d52df270d8a
Emmanuel Garnica <edgarnicap95@gmail.com>
2025-05-15 22:28:16 -0500

    feature: Adding barcode scan.
    Adding configurations and library for barcode scan and android and ios files generated.

--- src/app/features/qr/qr.page.ts
+++ src/app/features/qr/qr.page.ts
@@ -1,10 +1,41 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
 
 @Component({
   selector:'app-qr',
   templateUrl: './qr.page.html',
   standalone:false
 })
-export class QrPage{
+export class QrPage {
+  scanResult: string | null = null;
 
+  constructor() {}
+
+  async scan() {
+    // if (!Capacitor.isNativePlatform()) {
+    //   alert('Solo disponible en dispositivo móvil.');
+    //   return;
+    // }
+
+    try {
+
+      const result = await CapacitorBarcodeScanner.scanBarcode({
+        hint: 0,
+        scanInstructions: 'Acerca tu equipo al código QR',
+        scanText: 'Escaneando...',
+        cameraDirection: 1,
+        scanOrientation: 3,
+        scanButton: true,
+        web: {
+          showCameraSelection: true,
+        }
+      });
+
+      this.scanResult = result.ScanResult;
+
+    } catch (error) {
+      console.error('Error al escanear código:', error);
+    }
+  }
 }
